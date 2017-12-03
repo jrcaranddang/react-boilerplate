bdefb9f719488c2f94babed2cd1e35d366a332d4
jrcaranddang <jrcaranddang@gmail.com>
2017-12-03 13:50:46 -0800

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello world from Main.js
+      </div>
+    );
+  }
+}
+
+export default Main;
